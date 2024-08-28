f2f12a71386eb7344db666ec61ca52612df3bc13
IlhamTrinanda <ilhmtrnnda@gmail.com>
2024-08-29 00:25:21 +0700

    liner layout & text view

--- app/src/main/java/com/ilham/latihan2ilham/MainActivity.kt
+++ app/src/main/java/com/ilham/latihan2ilham/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.ilham.latihan2ilham
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnClick :Button
+    private lateinit var btnNextPage : Button
+    private lateinit var btnLatihan2 : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        btnClick = findViewById(R.id.btnClickHere)
+        btnNextPage = findViewById(R.id.btnNextPage)
+        btnLatihan2 = findViewById(R.id.btnLatihan2)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    btnClick.setOnClickListener(){
+        Toast.makeText(this,"Tekan Tombol",Toast.LENGTH_SHORT).show()
+    }
+    btnNextPage.setOnClickListener(){
+        val intentMenu2 = Intent(this@MainActivity, PageKeduaActivity::class.java)
+    }
+    btnLatihan2.setOnClickListener(){
+        val intentMenu3 = Intent(this@MainActivity, Latihan2Activity:: class.java)
+    }
+
+
 }
